68e6cbb84615191658623d8dd9e6666be0b8e511
DanielYakubov <Danielyak98@gmail.com>
2024-11-14 21:45:37 -0500

    added a significant amount of documentation; google styled google

--- data/merge_links.py
+++ data/merge_links.py
@@ -4,18 +4,15 @@ import pandas as pd
 
 if __name__ == "__main__":
     all_df = pd.read_csv("links/raw/extra_data_links.csv").filter(
-        items=["url", "status"], axis="columns"
-    )
+        items=["url", "status"], axis="columns")
     extra_non_phishing = all_df.loc[all_df["status"] == "legitimate"]
     extra_phishing = all_df.loc[all_df["status"] == "phishing"]
 
     # reading in the links, but specifying index column
-    non_phishing = pd.read_csv(
-        "links/raw/non_phishing.csv", index_col=0, names=["url"]
-    )
-    phishing = pd.read_csv(
-        "links/raw/phishing.csv", index_col=0, names=["url"]
-    )
+    non_phishing = pd.read_csv("links/raw/non_phishing.csv",
+                               index_col=0,
+                               names=["url"])
+    phishing = pd.read_csv("links/raw/phishing.csv", index_col=0, names=["url"])
 
     # adding status columns (this is the label)
     non_phishing["status"] = ["legitimate"] * len(non_phishing)
@@ -23,8 +20,7 @@ if __name__ == "__main__":
 
     # putting it all together
     all_data = pd.concat(
-        [non_phishing, phishing, extra_non_phishing, extra_phishing]
-    )
+        [non_phishing, phishing, extra_non_phishing, extra_phishing])
 
     # have some empty values in the dataset for some reason
     all_data.dropna(inplace=True)
--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,7 @@
+"""This module contains the functions and the code to preprocess the dataset"""
+
 import re
+from typing import Dict, Tuple
 from urllib.parse import urlparse
 
 import nltk
@@ -7,80 +10,101 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-#read and get the data
-def get_data(data_path):
-    df = pd.read_csv(data_path)
+def drop_nulls_and_duplicates(df: pd.DataFrame) -> pd.DataFrame:
+    """Removes nulls and duplicates from the df
+
+    Args:
+        df (pd.DataFrame): a generic pandas dataframe
+
+    Returns:
+        (pd.DataFrame) that has nulls and duplicates removed
+    """
+    df = df.dropna().reset_index(drop=True)
+    df = df.drop_duplicates(subset='URL', keep="first").reset_index(drop=True)
     return df
 
-#removing null and duplicate values
-def drop_nulls_and_duplicates(df):
-    df=df.dropna().reset_index(drop=True)
-    # df=df.drop_duplicates(keep='first').reset_index(drop=True)
-    df=df.drop_duplicates(subset='URL', keep="first").reset_index(drop=True)
-    return df
 
+def non_unique_text(df: pd.DataFrame) -> pd.DataFrame:
+    """Filter out texts that are not repeating in the dataset
 
-# #scaling of the colors - not needed now
-# def color_scaling(df):
-#     for i in range(len(df['red'])):
-#         df['red'][i] = df['red'][i] / 255
-#     for i in range(len(df['green'])):
-#         df['green'][i] = df['green'][i] / 255
-#     for i in range(len(df['blue'])):
-#         df['blue'][i] = df['blue'][i] / 255
-#     return df
+    Args:
+        df (pd.DataFrame): a generic pandas dataframe
 
-
-#filter out texts that are not repeating in the dataset
-def non_unique_text(df):
-    ty=pd.DataFrame(data=df['text'].value_counts().sort_values(ascending=False))
-    ty=ty.loc[ty['text'] != 1]
+    Returns:
+        (pd.DataFrame) with non-unique values
+    """
+    ty = pd.DataFrame(data=df['text'].value_counts().sort_values(
+        ascending=False))
+    ty = ty.loc[ty['text'] != 1]
 
     rem_index = []
     for i in range(len(ty)):
-        common_loc = df.loc[df['text'] == ty.index[i]]
         for j in range(len(df)):
             if df['text'][j] == ty.index[i]:
                 rem_index.append(j)
-        del common_loc 
 
-    df=df.drop(labels=rem_index,axis=0)
-    df=df.reset_index(drop=True)
+    df = df.drop(labels=rem_index, axis=0)
+    df = df.reset_index(drop=True)
     return df
 
 
-#removing data points whose url's do not exist or are non-functional
-def drop_bad_pages(df):
+def drop_bad_pages(df: pd.DataFrame) -> pd.DataFrame:
+    """Removing data points whose URLs do not exist or are non-functional
+
+    This is a heuristic way to filter out datapoints which correspond to websites without actual content.
+
+    Args:
+        df (pd.DataFrame): a generic pandas dataframe
+
+    Returns:
+        (pd.DataFrame) with errored out pages removed
+    """
     bad_pages = []
     bad_pattern = r"Site Not Found|This page isn’t working|Internal Server Error|This page isn’t working|Your connection is not private|404"
     for i in range(len(df)):
         if re.match(bad_pattern, df['text'][i], flags=re.IGNORECASE):
             bad_pages.append(i)
-    df=df.drop(labels=bad_pages,axis=0)
-    df=df.reset_index(drop=True)
+    df = df.drop(labels=bad_pages, axis=0)
+    df = df.reset_index(drop=True)
     return df
 
 
-#conversion of brands to numericals
-def one_hot_encoding(df):
+def one_hot_encoding(df: pd.DataFrame) -> pd.DataFrame:
+    """Conversion of brands to numerical values via one hot encoding
+
+    Args:
+        df (pd.DataFrame): a pandas DataFrame that contains a 'brand_name' target label.
+
+    Returns:
+        (pd.DataFrame) with one hot encoded brand name labels
+    """
     one_hot = pd.get_dummies(df.brand_name)
     df = pd.concat([df, one_hot], axis=1)
     return df
 
 
-#get the domain name
-def get_top_level_domain(url):
+def get_top_level_domain(url: str) -> str:
+    """Get the top level domain of a URL
+
+    Args:
+        url (str): a URL
+
+    Returns:
+        (str) the top level domain
+    """
     tld = urlparse(url)
     return tld.netloc
 
 
-#get the domain name's length
-def get_url_len(url_str):
-    return len(url_str)
+def get_url_char_bigram_probs() -> Tuple[float]:
+    """Get bigrams for each text(in our case it is n=2)
 
+    Args:
+         N/A
 
-#get n-grams for each text(in our case it is n=2)
-def get_url_char_bigram_probs():
+    Returns:
+        Tuple(bg_cnts, ung_cnts): the probability of bigrams and unigrams occuring in the text
+    """
     urls = pd.read_csv('data/links/top-1m.csv', names=['idx', 'URL'])
     urls = urls['URL']
     bg_cnts = {}
@@ -99,7 +123,8 @@ def get_url_char_bigram_probs():
 
         # adding unigram to count dictionary
         for unigram in chars:
-            ung_cnts[unigram] = ung_cnts[unigram] + 1 if unigram in ung_cnts else 1
+            ung_cnts[
+                unigram] = ung_cnts[unigram] + 1 if unigram in ung_cnts else 1
 
     # turning each count into a probability (MLE)
     for k, v in bg_cnts.items():
@@ -115,42 +140,75 @@ def get_url_char_bigram_probs():
     return bg_cnts, ung_cnts
 
 
-#score of each url texts' bigrams
-def get_score(ung_probs, bg_probs, url_str):
+def get_score(ung_probs: Dict[str, float], bg_probs: Dict[str, float],
+              url_str: str) -> float:
+    """Score of each url texts' bigrams
+
+    Args:
+        ung_probs (Dict[str, float]): unigram probabilities from the corpus
+        bg_probs (Dict[str, float]): bigram probabilities from the corpus
+        url_str (str): the url
+
+    Returns:
+        (float) a score of how likely the character sequence in the URL is
+    """
     url_chars = [c for c in url_str]
     url_bgs = nltk.bigrams(url_chars)
-    
+
     # getting a score of each url char bigram
     score = 0
     for bigram in url_bgs:
         # using linear interpolation smoothing (lambda 0.5)
         first_word, _ = bigram
-        score += 0.5*bg_probs.get(bigram, 0) + 0.5*ung_probs.get(first_word, 0)
+        score += 0.5 * bg_probs.get(bigram, 0) + 0.5 * ung_probs.get(
+            first_word, 0)
         # TODO probably should do entropy here
     return score
 
 
 #normal splitting of the dataset into training and testing
-def split_data(df):
+def split_data(df: pd.DataFrame, random_state=42) -> Tuple[np.array]:
+    """
+    Normal splitting of the dataset into training and testing
+
+    Args:
+        df (pd.DataFrame): a dataframe with a status columns
+        random_state (int): the random state to ensure consistent splits
+
+    Returns:
+        (Tuple[np.array]) the split dataset
+    """
     y = df['status']
     X = df.drop(['status'], axis=1)
-    X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=0.1, random_state=42)
+    X_tr, X_te, y_tr, y_te = train_test_split(X,
+                                              y,
+                                              test_size=0.1,
+                                              random_state=random_state)
     return X_tr, X_te, y_tr, y_te
 
 
-#store csv's
-def store_datasets(Xtrain,Xtest,ytrain,ytest):
-    Xtrain.to_csv('data/X_train.csv',index=False)
-    Xtest.to_csv('data/X_test.csv',index=False)
-    ytrain.to_csv('data/y_train.csv',index=False)
-    ytest.to_csv('data/y_test.csv',index=False)
+def store_datasets(x_train: np.array, x_test: np.array, y_train: np.array,
+                   y_test: np.array) -> None:
+    """Save the CSVs of the split data to the data directory
+
+    Args:
+        x_train (np.array): training split of X labels
+        x_test (np.array): test split of X labels
+        y_train (np.array): training split of y labels
+        y_test (np.array): testing split of y labels
+
+    Returns:
+        None, saves the values to data/ directory
+    """
+    x_train.to_csv('data/X_train.csv', index=False)
+    x_test.to_csv('data/X_test.csv', index=False)
+    y_train.to_csv('data/y_train.csv', index=False)
+    y_test.to_csv('data/y_test.csv', index=False)
 
 
 if __name__ == "__main__":
-    data = get_data("data/scraped/all_data_scraped.csv")
-    # data['status'] = np.where(data['status']=="legitimate",0,1)
+    data = pd.read_csv("data/scraped/all_data_scraped.csv")
     data = drop_nulls_and_duplicates(data)
-    # data = color_scaling(data)
     data = non_unique_text(data)
     data = drop_bad_pages(data)
     data = one_hot_encoding(data)
@@ -158,13 +216,13 @@ if __name__ == "__main__":
 
     # URL based features
     bg_probs, ung_probs = get_url_char_bigram_probs()
-    url_lens = []
+    tld_lens = []
     url_scores = []
     for url in data['URL']:
         tld = get_top_level_domain(url)
-        l = get_url_len(tld)
+        tld_len = len(tld)  # the len of the tld is a feature
         score = get_score(ung_probs, bg_probs, tld)
-        url_lens.append(l)
+        tld_lens.append(tld_len)
         url_scores.append(score)
 
     # text based features
@@ -173,7 +231,7 @@ if __name__ == "__main__":
         text_lens.append(len(t))
 
     # adding new features to data
-    data['url_len'] = url_lens
+    data['url_len'] = tld_lens
     data['tld_char_score'] = url_scores
     data['text_len'] = text_lens
     # dropping all text features
